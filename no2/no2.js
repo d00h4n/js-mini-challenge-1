f994282769acaf44167ae61275ebf6213b062ea6
d00h4n <108811042+d00h4n@users.noreply.github.com>
2022-07-29 21:46:32 +0700

    Update no2.js

--- no2/no2.js
+++ no2/no2.js
@@ -1,20 +1,20 @@
 function pizzaMozarella(score) {
   // Tulis kodingan dibawah ini
 
-  for (let i = 1; i<=15; i++){
-    if (i % 3 ===0) {console.log('Pizza');
-  }else if (i % 5 ===0) {console.log('Mozarella');
-  }else{
-      console.log(i)
-    }
+  for (let i = 1; i<=score; i++){
+    if (i % 3 ===0 && i % 5 ===0) {console.log('PizzaMozarela');
+  }else if (i % 3 ===0) {console.log('Pizza');
+  }else if (i % 5 ===0) {console.log('Mozzarela') 
+  }else{console.log(i)}
+  
   }
 
-  result = 'score';
+  result = 'Pizza';
   return result;
 }
 
 // Test Case
-pizzaMozarella(15);
+pizzaMozarella(20);
 // 1
 // 2
 // Pizza
@@ -30,3 +30,8 @@ pizzaMozarella(15);
 // 13 
 // 14
 // PizzaMozarella
+// 16
+// 17
+// Pizza
+// 19
+// Mozarella
